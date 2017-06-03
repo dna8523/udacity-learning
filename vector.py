817557c1be27b30f0247189ca5f798b285879194
dna8523 <dna8523@gmail.com>
2017-06-03 09:03:03 +0000

    Update vector.py

--- vector.py
+++ vector.py
@@ -1,3 +1,5 @@
+import math
+
 class Vector(object):
     def __init__(self, coordinates):
         try:
@@ -19,3 +21,29 @@ class Vector(object):
 
     def __eq__(self, v):
         return self.coordinates == v.coordinates
+
+    def plus(self,v):
+        new_coordinates = [x + y for x,y in zip(self.coordinates,v.coordinates)]
+        return Vector(new_coordinates)
+
+    def subtract(self,v):
+        new_coordinates = [x-y for x,y in zip(self.coordinates,v.coordinates)]
+        return Vector(new_coordinates)
+
+    def scalar_multiply(self,c):
+        new_coordinates = [c * x for x in self.coordinates]
+        return Vector(new_coordinates)
+
+    def magnitude(self):
+        sums = 0
+        for x in self.coordinates:
+            sums += x**2
+        return math.sqrt(sums)
+
+    def normalize(self):
+        new_coordinates = []
+        for x in self.coordinates:
+            new_coordinates.append((1. / self.magnitude()) * x)
+        return Vector(new_coordinates)
+a = Vector([1.996,3.108,-4.554])
+print a.normalize()
