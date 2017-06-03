c9322e73766af6e2fa55c7619f5fb85bf94bb144
dna8523 <dna8523@gmail.com>
2017-06-03 08:13:02 +0000

    Update vector.py

--- vector.py
+++ vector.py
@@ -1,17 +1,21 @@
 class Vector(object):
-    def __inti__(self, coordinates):
+    def __init__(self, coordinates):
         try:
             if not coordinates:
                 raise ValueError
             self.coordinates = tuple(coordinates)
             self.dimension = len(coordinates)
 
-       except ValueError:
-           raise ValueError('The coordinates must be nonempty')
-       except TypeError:
-           raise TypeError('The coordinates must be interable')
-    def __str__(self):
-        return 'Vector:{}'.format(self.coordinates)
+        except ValueError:
+            raise ValueError('The coordinates must be nonempty')
 
-    def __eq__(self,v):
+        except TypeError:
+            raise TypeError('The coordinates must be an iterable')
+
+
+    def __str__(self):
+        return 'Vector: {}'.format(self.coordinates)
+
+
+    def __eq__(self, v):
         return self.coordinates == v.coordinates
