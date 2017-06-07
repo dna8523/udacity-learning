b47399f1b39c01d5280cbb2357deaa4906489ace
dna8523 <dna8523@gmail.com>
2017-06-07 09:43:12 +0800

    Update vector.py

--- vector.py
+++ vector.py
@@ -1,5 +1,6 @@
 import math
 
+
 class Vector(object):
     def __init__(self, coordinates):
         try:
@@ -14,23 +15,23 @@ class Vector(object):
         except TypeError:
             raise TypeError('The coordinates must be an iterable')
 
-
     def __str__(self):
         return 'Vector: {}'.format(self.coordinates)
-
 
     def __eq__(self, v):
         return self.coordinates == v.coordinates
 
-    def plus(self,v):
-        new_coordinates = [x + y for x,y in zip(self.coordinates,v.coordinates)]
+    def plus(self, v):
+        new_coordinates = [x + y for x,
+                           y in zip(self.coordinates, v.coordinates)]
         return Vector(new_coordinates)
 
-    def subtract(self,v):
-        new_coordinates = [x-y for x,y in zip(self.coordinates,v.coordinates)]
+    def subtract(self, v):
+        new_coordinates = [x - y for x,
+                           y in zip(self.coordinates, v.coordinates)]
         return Vector(new_coordinates)
 
-    def scalar_multiply(self,c):
+    def scalar_multiply(self, c):
         new_coordinates = [c * x for x in self.coordinates]
         return Vector(new_coordinates)
 
@@ -46,14 +47,40 @@ class Vector(object):
             new_coordinates.append((1. / self.magnitude()) * x)
         return Vector(new_coordinates)
 
-    def dot_product(self,v):
-        return sum([x*y for x,y in zip(self.coordinates,v.coordinates)])
+    def dot_product(self, v):
+        return sum([x * y for x, y in zip(self.coordinates, v.coordinates)])
 
-    def angle(self,v):
+    def angle(self, v):
         if self.magnitude() == 0 or v.magnitude() == 0:
             raise ValueError('zero vector detected')
-        result = math.acos(self.dot_product(v)/(self.magnitude()*v.magnitude()))
-        return result,math.degrees(result)
-a = Vector([7.35,0.221,5.188])
-b = Vector([2.751,8.259,3.985])
-print a.angle(b)
+        result = math.acos(self.dot_product(
+            v) / (self.magnitude() * v.magnitude()))
+        return result, math.degrees(result)
+
+    def project(self, basis):
+        u = basis.normalize()
+        t = self.dot_product(u)
+        return u.scalar_multiply(t)
+
+    def orthognal(self, basis):
+        parallel = self.project(basis)
+        return v.subtract(parallel)
+
+    def crossproduct(self, v):
+        new_coordinates = []
+        new_coordinates.append(
+            self.coordinates[1] * v.coordinates[2] - self.coordinates[2] * v.coordinates[1])
+        new_coordinates.append(
+            -(self.coordinates[0] * v.coordinates[2] - self.coordinates[2] * v.coordinates[0]))
+        new_coordinates.append(
+            self.coordinates[0] * v.coordinates[1] - self.coordinates[1] * v.coordinates[0])
+        return Vector(new_coordinates)
+
+    def area_parallel(self, v):
+        height = self.orthognal(v)
+        return height.magnitude() * v.magnitude(), 'triangle = ', height.magnitude() * v.magnitude() / 2
+
+
+v = Vector([1.5, 9.547, 3.691])
+b = Vector([-6.007, 0.124, 5.772])
+print v.area_parallel(b)
