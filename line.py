d11a7d6f850c849523adad0decde572d45a59ece
dna8523 <dna8523@gmail.com>
2017-06-14 16:09:07 +0800

    Update line.py

--- line.py
+++ line.py
@@ -1,5 +1,5 @@
 from decimal import Decimal, getcontext
-
+from math import *
 from vector import Vector
 
 getcontext().prec = 30
@@ -18,22 +18,23 @@ class Line(object):
         self.normal_vector = normal_vector
 
         if not constant_term:
-            constant_term = Decimal('0')
-        self.constant_term = Decimal(constant_term)
+            constant_term = 0
+        self.constant_term = constant_term
 
-        self.set_basepoint()
+        self.basepoint = self.set_basepoint()
 
     def set_basepoint(self):
         try:
             n = self.normal_vector
             c = self.constant_term
-            basepoint_coords = ['0'] * self.dimension
+            basepoint_coords = [0] * self.dimension
 
             initial_index = Line.first_nonzero_index(n)
             initial_coefficient = n[initial_index]
 
             basepoint_coords[initial_index] = c / initial_coefficient
             self.basepoint = Vector(basepoint_coords)
+            return self.basepoint
 
         except Exception as e:
             if str(e) == Line.NO_NONZERO_ELTS_FOUND_MSG:
@@ -92,13 +93,26 @@ class Line(object):
         return n1.is_parallel(n2)
 
     def __eq__(self, line):
-        p1 = Vector([0, self.constant_term / self.normal_vector[1]])
-        p2 = Vector([line.constant_term / line.normal_vector[0], 0])
-        v = p1.subtract(p2)
-        return self.is_parallel_line(line) and v.is_orthognal(Vector([self.normal_vector]))
+        if not self.is_parallel_line(line):
+            return False
+        x0 = self.basepoint
+        y0 = line.basepoint
+        basepoint_difference = x0.subtract(y0)
+        n = Vector(self.normal_vector)
+        return basepoint_difference.is_orthognal(n)
 
-    def intersection(self,line):
-        
+    def intersection(self, line):
+        if self.is_parallel_line(line):
+            return 'None'
+        a = self.normal_vector[0]
+        b = self.normal_vector[1]
+        c = line.normal_vector[0]
+        d = line.normal_vector[1]
+        k1 = self.constant_term
+        k2 = line.constant_term
+        x = (d * k1 - b * k2) / (a * d - b * c)
+        y = (a * k2 - c * k1) / (a * d - b * c)
+        return Vector([x, y])
 
     @staticmethod
     def first_nonzero_index(iterable):
@@ -113,6 +127,9 @@ class MyDecimal(Decimal):
         return abs(self) < eps
 
 
-l1 = Line([1, 1], 1)
-l2 = Line([2, 2], 2)
-print l1.is_parallel_line(l2)
+l1 = Line([1.182, 5.562], 6.744)
+l2 = Line([1.773, 8.343], 9.525)
+v = l1.basepoint.subtract(l2.basepoint)
+n1 = Vector(l1.normal_vector)
+n2 = Vector(l2.normal_vector)
+print l1 == l2, l1.intersection(l2), l1.is_parallel_line(l2)
