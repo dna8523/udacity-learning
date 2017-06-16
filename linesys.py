b2df6f85bfde91f9f1047a2685499f1921ec7e26
dna8523 <dna8523@gmail.com>
2017-06-16 17:46:45 +0800

    Update linesys.py

--- linesys.py
+++ linesys.py
@@ -31,14 +31,18 @@ class LinearSystem(object):
         self.planes[row2] = t
 
     def multiply_coefficient_and_row(self, coefficient, row):
-        t = self.planes[row].normal_vector
-        for i in t:
-            t[i] *= coefficient
-        self.planes[row].constant_term = coefficient * \
-            self.planes[row].constant_term
+        t, c = self.planes[row].normal_vector, self.planes[row].constant_term
+        for i in range(len(t)):
+            self.planes[row].normal_vector[i] = coefficient * t[i]
+        self.planes[row].constant_term = coefficient * c
 
     def add_multiple_times_row_to_row(self, coefficient, row_to_add, row_to_be_added_to):
-        pass  # add your code here
+        t, c = self.planes[row_to_add].normal_vector, self.planes[row_to_add].constant_term
+        t1, c1 = self.planes[row_to_be_added_to].normal_vector, self.planes[row_to_be_added_to].constant_term
+        for i in range(len(t)):
+            self.planes[row_to_be_added_to].normal_vector[i] = t[i] * \
+                coefficient + t1[i]
+        self.planes[row_to_be_added_to].constant_term = c * coefficient + c1
 
     def indices_of_first_nonzero_terms_in_each_row(self):
         num_equations = len(self)
@@ -82,3 +86,62 @@ class LinearSystem(object):
 class MyDecimal(Decimal):
     def is_near_zero(self, eps=1e-10):
         return abs(self) < eps
+
+
+p0 = Plane([1, 1, 1], 1)
+p1 = Plane([0, 1, 0], 2)
+p2 = Plane([1, 1, -1], 3)
+p3 = Plane([1, 0, -2], 2)
+
+s = LinearSystem([p0, p1, p2, p3])
+s.swap_rows(0, 1)
+if not (s[0] == p1 and s[1] == p0 and s[2] == p2 and s[3] == p3):
+    print 'test case 1 failed'
+
+s.swap_rows(1, 3)
+if not (s[0] == p1 and s[1] == p3 and s[2] == p2 and s[3] == p0):
+    print 'test case 2 failed'
+
+s.swap_rows(3, 1)
+if not (s[0] == p1 and s[1] == p0 and s[2] == p2 and s[3] == p3):
+    print 'test case 3 failed'
+
+s.multiply_coefficient_and_row(1, 0)
+if not (s[0] == p1 and s[1] == p0 and s[2] == p2 and s[3] == p3):
+    print 'test case 4 failed'
+
+s.multiply_coefficient_and_row(-1, 2)
+if not (s[0] == p1 and
+        s[1] == p0 and
+        s[2] == Plane([-1, -1, 1], -3) and
+        s[3] == p3):
+    print 'test case 5 failed'
+
+s.multiply_coefficient_and_row(10, 1)
+if not (s[0] == p1 and
+        s[1] == Plane([10, 10, 10], 10) and
+        s[2] == Plane([-1, -1, 1], -3) and
+        s[3] == p3):
+    print 'test case 6 failed'
+
+s.add_multiple_times_row_to_row(0, 0, 1)
+if not (s[0] == p1 and
+        s[1] == Plane([10, 10, 10], 10) and
+        s[2] == Plane([-1, -1, 1], -3) and
+        s[3] == p3):
+    print 'test case 7 failed'
+
+s.add_multiple_times_row_to_row(1, 0, 1)
+if not (s[0] == p1 and
+        s[1] == Plane([10, 11, 10], 12) and
+        s[2] == Plane([-1, -1, 1], -3) and
+        s[3] == p3):
+    print 'test case 8 failed'
+
+s.add_multiple_times_row_to_row(-1, 1, 0)
+if not (s[0] == Plane([-10, -10, -10], -10) and
+        s[1] == Plane([10, 11, 10], 12) and
+        s[2] == Plane([-1, -1, 1], -3) and
+        s[3] == p3):
+    print 'test case 9 failed'
+print s[0], '\n', Plane([-10, -10, -10], -10), '\n', s[0] == Plane([-10, -10, -10], -10), '\n', s[0].is_parallel_plane(Plane([-10, -10, -10], -10))
