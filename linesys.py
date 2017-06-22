7ae1f0654054ab69a004e79f4a88eeb2c51b0004
dna8523 <dna8523@gmail.com>
2017-06-22 17:55:24 +0800

    Update linesys.py

--- linesys.py
+++ linesys.py
@@ -44,6 +44,25 @@ class LinearSystem(object):
         c1 += c * coefficient
         self.planes[row_to_be_added_to] = Plane(t1, c1)
 
+    def compute_triangular_form(self):
+        system = deepcopy(self)
+        indices = system.indices_of_first_nonzero_terms_in_each_row()
+        for i,c in enumerate(indices) :
+            top_index = system.topmost_index(i)
+            if c != i:
+                if top_index != -1:
+                    system.swap_rows(i,top_index)
+                   
+    def topmost_index(self,idex):
+        indices = self.indices_of_first_nonzero_terms_in_each_row()
+        result = -1
+        for i, c in enumerate(indices):
+            if c == idex:
+                result = i
+                break
+        
+        return result
+
     def indices_of_first_nonzero_terms_in_each_row(self):
         num_equations = len(self)
         num_variables = self.dimension
@@ -88,59 +107,40 @@ class MyDecimal(Decimal):
         return abs(self) < eps
 
 
-p0 = Plane([1, 1, 1], 1)
-p1 = Plane([0, 1, 0], 2)
-p2 = Plane([1, 1, -1], 3)
-p3 = Plane([1, 0, -2], 2)
-
-s = LinearSystem([p0, p1, p2, p3])
-s.swap_rows(0, 1)
-if not (s[0] == p1 and s[1] == p0 and s[2] == p2 and s[3] == p3):
+p1 = Plane([1, 1, 1], 1)
+p2 = Plane([0, 1, 1], 2)
+s = LinearSystem([p1, p2])
+t = s.compute_triangular_form()
+if not (t[0] == p1 and
+        t[1] == p2):
     print 'test case 1 failed'
 
-s.swap_rows(1, 3)
-if not (s[0] == p1 and s[1] == p3 and s[2] == p2 and s[3] == p0):
+p1 = Plane([1, 1, 1], 1)
+p2 = Plane([1, 1, 1], 2)
+s = LinearSystem([p1, p2])
+t = s.compute_triangular_form()
+if not (t[0] == p1 and
+        t[1] == Plane(constant_term=1)):
     print 'test case 2 failed'
 
-s.swap_rows(3, 1)
-if not (s[0] == p1 and s[1] == p0 and s[2] == p2 and s[3] == p3):
+p1 = Plane([1, 1, 1], 1)
+p2 = Plane([0, 1, 0], 2)
+p3 = Plane([1, 1, -1], 3)
+p4 = Plane([1, 0, -2], 2)
+s = LinearSystem([p1, p2, p3, p4])
+t = s.compute_triangular_form()
+if not (t[0] == p1 and
+        t[1] == p2 and
+        t[2] == Plane([0, 0, -2], 2)
+        t[3] == Plane()):
     print 'test case 3 failed'
 
-s.multiply_coefficient_and_row(1, 0)
-if not (s[0] == p1 and s[1] == p0 and s[2] == p2 and s[3] == p3):
+p1 = Plane([0, 1, 1], 1)
+p2 = Plane([1, -1, 1], 2)
+p3 = Plane([1, 2, -5], 3)
+s = LinearSystem([p1, p2, p3])
+t = s.compute_triangular_form()
+if not (t[0] == Plane([1, -1, 1]), 2 and
+        t[1] == Plane([0, 1, 1], 1) and
+        t[2] == Plane([0, 0, -9], -2):
     print 'test case 4 failed'
-
-s.multiply_coefficient_and_row(-1, 2)
-if not (s[0] == p1 and
-        s[1] == p0 and
-        s[2] == Plane([-1, -1, 1], -3) and
-        s[3] == p3):
-    print 'test case 5 failed'
-
-s.multiply_coefficient_and_row(10, 1)
-if not (s[0] == p1 and
-        s[1] == Plane([10, 10, 10], 10) and
-        s[2] == Plane([-1, -1, 1], -3) and
-        s[3] == p3):
-    print 'test case 6 failed'
-
-s.add_multiple_times_row_to_row(0, 0, 1)
-if not (s[0] == p1 and
-        s[1] == Plane([10, 10, 10], 10) and
-        s[2] == Plane([-1, -1, 1], -3) and
-        s[3] == p3):
-    print 'test case 7 failed'
-
-s.add_multiple_times_row_to_row(1, 0, 1)
-if not (s[0] == p1 and
-        s[1] == Plane([10, 11, 10], 12) and
-        s[2] == Plane([-1, -1, 1], -3) and
-        s[3] == p3):
-    print 'test case 8 failed'
-
-s.add_multiple_times_row_to_row(-1, 1, 0)
-if not (s[0] == Plane([-10, -10, -10], -10) and
-        s[1] == Plane([10, 11, 10], 12) and
-        s[2] == Plane([-1, -1, 1], -3) and
-        s[3] == p3):
-    print 'test case 9 failed'
