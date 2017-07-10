049ebe7afeab9848e3044dc860a5d2f42533253b
dna8523 <dna8523@gmail.com>
2017-07-10 17:58:26 +0800

    Update linearRegression.py

--- linearRegression.py
+++ linearRegression.py
@@ -1,1 +1,11 @@
 def linearRegression(points):
+    X, Y = [], []
+    for i in range(len(points)):
+        Y.append([])
+        Y[i].append(points[i][1])
+        X.append([])
+        X[i].append(points[i][0])
+        X[i].append(1)
+    b = matxMultiply(transpose(X), Y)
+    A = matxMultiply(transpose(X), X)
+    return gj_Solve(A, b)
